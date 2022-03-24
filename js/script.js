adfdc4251a8b988c2b597db2a79f84182b384889
gilcastellar <gilcastellar@gmail.com>
2022-03-24 12:32:25 -0300

    added functional + and - buttons

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function addOne () {
+    number += 1;
+    document.getElementById("episode").innerHTML = number;
+}
+
+function decreaseOne () {
+    number -= 1;
+    document.getElementById("episode").innerHTML = number;
+}
