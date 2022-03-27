8b914789459fadbf95348f89af64d37e0e519449
gilcastellar <gilcastellar@gmail.com>
2022-03-27 15:28:18 -0300

    lots of bug fixes, mostly functional

--- js/script.js
+++ js/script.js
@@ -1,26 +1,47 @@
+function calcRemaining (row) {
+    progress = document.getElementById("row" + row + "Progress").innerHTML;
+    total = document.getElementById("row" + row + "Total").innerHTML;
+    remaining = total - progress;
+    document.getElementById("row" + row + "Remaining").innerHTML = remaining;
+    updateRemainingTotal(remaining);
+}
+
+function updateRemainingTotal(valueToAdd) {
+    if (document.getElementById("total-remaining-value").innerHTML == '') {
+        remainingEpis = valueToAdd;
+        document.getElementById("total-remaining-value").innerHTML = remainingEpis;
+        document.getElementById("total-remaining-text").innerHTML = "Episodes remanining<br>until completion."
+    }
+    else {
+        var previous = document.getElementById("total-remaining-value").innerHTML;
+        document.getElementById("total-remaining-value").innerHTML = parseInt(previous) + parseInt(valueToAdd);
+    }
+}
+
 function addOne (row) {
-    number = document.getElementById("row" + row + "Progress").innerHTML;
-    number = parseInt(number);
-    number += 1;
-    document.getElementById("row" + row + "Progress").innerHTML = number;
+    progress = document.getElementById("row" + row + "Progress").innerHTML;
+    progress = parseInt(progress);
+    total = document.getElementById("row" + row + "Total").innerHTML;
+    if (progress < total) {
+        remainingEpis -= 1;
+        updateRemainingTotal(-1);
+        progress += 1;
+        document.getElementById("row" + row + "Progress").innerHTML = progress;
+    }
 }
 
 function decreaseOne (row) {
-    number = document.getElementById("row" + row + "Progress").innerHTML;
-    number = parseInt(number);
-    number -= 1;
-    document.getElementById("row" + row + "Progress").innerHTML = number;
+    progress = document.getElementById("row" + row + "Progress").innerHTML;
+    progress = parseInt(progress);
+    if (progress > 0) {
+        remainingEpis += 1;
+        updateRemainingTotal(1);
+        progress -= 1;
+        document.getElementById("row" + row + "Progress").innerHTML = progress;
+    }
 }
 
-function addNewDiv () {
-    var newDiv = "<div id='newDiv";
-    newDiv += row;
-    newDiv += "'></div>";
-
-    document.body.innerHTML = document.body.innerHTML + newDiv;
-}
-
-function genTableRow () {
+function newAnime () {
     row += 1;
 
     //addNewDiv();
@@ -28,25 +49,31 @@ function genTableRow () {
     var newDiv = document.createElement("div");
     newDiv.id = 'row' + row;
 
-    var html = "<table class='table'><tbody><tr><td>";
+    var html = '';
+
+    html += "<table class='table table-bordered anime-table-row'><tbody><tr><td id='anime-row-name' style='width: 300px'>";
     html += document.getElementById("inputAnimeName").value;
-    html += "</td><td id='decrease-btn'><button type='button' value='input' onclick='decreaseOne(";
+    html += "</td><td style='width: 50px' id='decrease-btn'><button style='width: 30px' type='button' value='input' onclick='decreaseOne(";
     html += row;
-    html += ")'>-</button></td><td id='row";
+    html += ")'>-</button></td><td style='width: 100px' id='row";
     html += row;
     html += "Progress'>";
     html += document.getElementById("inputProgress").value;
-    html += "</td><td id='increase-btn'><button type='button' value='input' onclick='addOne(";
+    html += "</td><td style='width: 50px' id='increase-btn'><button style='width: 30px' type='button' value='input' onclick='addOne(";
     html += row;
-    html += ")'>+</button></td><td id='row";
+    html += ")'>+</button></td><td style='width: 80px' id='row";
     html += row;
     html += "Total'>";
     html += document.getElementById("inputTotalEpisodes").value;
-    html += "</td></tr></tbody></table>";
+    html += "</td><td style='width: 110px' class='remaining-value' id='row";
+    html += row;
+    html += "Remaining'></td></tr></tbody></table>";
     
     newDiv.innerHTML = html;
 
     document.body.appendChild(newDiv);
+
+    calcRemaining(row);
 
     //</tr></tbody></table>";
 
